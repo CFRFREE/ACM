8d3d279ea9e4b774c449f18886054cb3f3f040e7
CFRFREE <60842418+CFRFREE@users.noreply.github.com>
2022-09-05 09:18:22 +0800

    master

--- 模板/组合数.cpp
+++ 模板/组合数.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define P 1000000007
-#define INF ((1<<31)-1)
-#define INFF ((1<<63)-1)
+#define INF ((1 << 31) - 1)
+#define INFF ((1 << 63) - 1)
 #define int long long
 #define N 1000003
 using namespace std;
@@ -15,18 +15,19 @@ inline int read()
 		w |= ch == '-';
 		ch = getchar();
 	}
-	while (isdigit(ch)) X = (X << 3) + (X << 1) + (ch ^ 48), ch = getchar();
+	while (isdigit(ch))
+		X = (X << 3) + (X << 1) + (ch ^ 48), ch = getchar();
 	return w ? -X : X;
 }
 int qpow(int x, int y)
 {
 	int res = 1ll;
-	x %= k;
+	x %= P;
 	while (y)
 	{
 		if (y & 1ll)
-			res = res * x % k;
-		x = x * x % k;
+			res = res * x % P;
+		x = x * x % P;
 		y >>= 1ll;
 	}
 	return res;
@@ -36,7 +37,7 @@ void init()
 	jc[0] = 1;
 	for (int i = 1; i < N; i++)
 		jc[i] = (jc[i - 1] * i) % P;
-	jc_inv[N - 1] = qpow(jc[N - 1], P - 2, P);
+	jc_inv[N - 1] = qpow(jc[N - 1], P - 2);
 	for (int i = N - 1; i >= 1; i--)
 		jc_inv[i - 1] = (jc_inv[i] * i) % P;
 }
@@ -46,7 +47,7 @@ int C(int x, int y)
 		return 0;
 	if (y == 0)
 		return 1;
-	return	(jc[x] * jc_inv[y] % P) * jc_inv[x - y] % P;
+	return (jc[x] * jc_inv[y] % P) * jc_inv[x - y] % P;
 }
 signed main()
 {
